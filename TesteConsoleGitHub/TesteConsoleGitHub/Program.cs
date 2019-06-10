4f281210713711da9a2bb8c16cb36d575ba9e3dd
Marcos Moraes <mdmoraes.mopsus@gmail.com>
2019-06-10 19:12:43 -0300

    Primeiro Commit

---
+++ TesteConsoleGitHub/TesteConsoleGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsoleGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste no GitHub");
+            Console.ReadKey();
+        }
+    }
+}
